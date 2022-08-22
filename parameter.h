7db7cd9ea765b93785737f4a4f451b4d6c3d73dc
dyk <2410699227@qq.com>
2022-08-22 17:29:58 +0800

    致死个体筛选

--- parameter.h
+++ parameter.h
@@ -10,7 +10,7 @@ const bool AbsoluteError = true;
 const double RANGE = 100.0;
 // 可选函数集: +、-、*、/、<、 >、&(AND)、|(OR)、 X(XOR)、S(sin)、C(cos)、T(tan)、E(exp)、L(log10)、Q(sqrt)
 const char Function[] = {'-', '+', '*', '/'}; //,'>','<','&','|','X','^'};
-const char Terminator[] = {'a','b','c','d'};//,'e','f','g','h','i','j'};
+const char Terminator[] = {'a','b','c','d','e','f','g','h','i','j'};
 const bool Allow_Single_Gene = false;   //是否支持单符号基因
 /* 分类阈值*/
 const bool CLASSIFICATION = true;
