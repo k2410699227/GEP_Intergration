e0d6b790dc9307cae4d0d3924091cde9a7e2cfcf
dyk <2410699227@qq.com>
2022-08-22 16:23:12 +0800

    Algerian_forest数据集

---
+++ data/parse_forestfire.py
@@ -0,0 +1,14 @@
+import pandas
+
+origin = open("Algerian_forest_fires_dataset_UPDATE.csv", 'r+')
+inputFile = open("input.txt", "w+")
+outputFile = open("output.txt", "w+")
+evaluationFile = open("evaluation.txt", "w+")
+classes = dict({'fire': 1, 'not fire': 0})
+
+for i, o in enumerate(origin.readlines()):
+    line = o.strip().split(',')
+    if (i + 1) % 5 == 0:
+        print(' '.join(line[3:-1]), classes[line[-1]], file=evaluationFile)
+    else:
+        print(' '.join(line[3:-1]), file=inputFile)
