fa81cfa7046842f85032483a38b0f56298358ea2
dyk <2410699227@qq.com>
2022-08-22 17:35:09 +0800

    验证脚本更新

--- data/evaluation.py
+++ data/evaluation.py
@@ -5,7 +5,7 @@ count = 0
 for e in evaluationFile.readlines():
     indep = [eval(k) for k in e.strip().split()]
     dependent = indep[-1]
-    a,b,c,d = indep[0:10]
+    a,b,c,d,e,f,g,h,i,j = indep[0:10]
     # print(a,b,c,d)
     result = (((b-d)/(c*b))/(c/a)) + (((b+d)/b)-(c*c)) + ((d-b)/(b+d)) + ((a-(b-c))-c) + (a-(d/(a*a)))
     # result = indep[1]-indep[2]
