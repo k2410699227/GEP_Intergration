92306545fbbbb68abadb8689f36a27af5c687cb9
dyk <2410699227@qq.com>
2022-08-23 08:56:37 +0800

    更新验证脚本

--- data/evaluation.py
+++ data/evaluation.py
@@ -2,16 +2,21 @@ import os
 
 evaluationFile = open('evaluation.txt','r+')
 count = 0
-for e in evaluationFile.readlines():
-    indep = [eval(k) for k in e.strip().split()]
+invalid = 0
+for line in evaluationFile.readlines():
+    indep = [eval(k) for k in line.strip().split()]
     dependent = indep[-1]
     a,b,c,d,e,f,g,h,i,j = indep[0:10]
-    # print(a,b,c,d)
-    result = (((b-d)/(c*b))/(c/a)) + (((b+d)/b)-(c*c)) + ((d-b)/(b+d)) + ((a-(b-c))-c) + (a-(d/(a*a)))
-    # result = indep[1]-indep[2]
-    # print(result)
-    if result > 0.5 and dependent == 1:
-        count += 1
-    elif result <= 0.5 and dependent == 0:
-        count += 1
-print(count)
+    
+    try:
+        result = (j/j) + ((i/e)+j) + (j-c) + (h*j) + (h-j)
+
+        if result > 0.5 and dependent == 1:
+            count += 1
+        elif result <= 0.5 and dependent == 0:
+            count += 1
+    except ZeroDivisionError:
+        invalid += 1
+        print(' '.join(line.strip().split()))
+
+print("预测正确样本数：",count,"无效样本数：",invalid)
