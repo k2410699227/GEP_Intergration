56c31defd5efe7494fe4cbce5e252c35e6cc90bc
dyk <2410699227@qq.com>
2022-09-19 16:01:05 +0800

    扩大验证集数量

--- data/parseForestFire.py
+++ data/parseForestFire.py
@@ -8,7 +8,7 @@ classes = dict({'fire': 1, 'not fire': 0})
 
 for i, o in enumerate(origin.readlines()):
     line = o.strip().split(',')
-    if (i + 1) % 5 == 0:
+    if (i + 1) % 4 == 0:
         print(' '.join(line[3:-1]), classes[line[-1]], file=evaluationFile)
     else:
         print(' '.join(line[3:-1]), file=inputFile)
