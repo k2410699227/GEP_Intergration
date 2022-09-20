0df9998b5b0031b5234938b4c4ba728cdc23d510
dyk <2410699227@qq.com>
2022-09-20 19:58:36 +0800

    试验脚本更新

--- run.py
+++ run.py
@@ -1,23 +1,32 @@
 import matplotlib.pyplot as plt
-import os,numpy
+import os
+import numpy
+import argparse
 
 duration = []
 accuracy = []
-round = 100
+round = 1
+parser = argparse.ArgumentParser(description="your script description")        
 
-for i in range(0,round):
-    print(i+1,end='\n')
+parser.add_argument('-r', type= int)
+args = parser.parse_args()
+if args.r:
+    round = args.r
+
+
+for i in range(0, round):
+    print(i+1, end='\n')
     lines = os.popen('./GeneExpressionProgramming').readlines()
     accuracy.append(eval(lines[-1].split()[-1].split('%')[0])/100)
     duration.append(eval(lines[-3].split()[-1]))
 
-order = numpy.arange(1,round+1)
-
-plt.plot(order,accuracy)
+order = numpy.arange(1, round+1)
+print(accuracy, duration,sep='\n')
+plt.plot(order, accuracy)
 plt.title('Accuracy of test')
 plt.savefig('accuracy-{}.png'.format(round))
 
 plt.close()
-plt.plot(order,duration)
+plt.plot(order, duration)
 plt.title('Duration of test')
 plt.savefig('duration-{}.png'.format(round))
