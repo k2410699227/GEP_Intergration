cbc041f1b5bd18335eb6247d8384db040e13c60d
dyk <2410699227@qq.com>
2022-09-27 15:42:45 +0800

    测试脚本更新

--- run.py
+++ run.py
@@ -24,8 +24,8 @@ else:
 for i in range(0, round):
     print(i+1, end='\n')
     lines = os.popen('./GeneExpressionProgramming'+acc).readlines()
-    accuracy.append(eval(lines[-1].split()[-1].split('%')[0])/100)
-    duration.append(eval(lines[-3].split()[-1]))
+    accuracy.append(eval(lines[-2].split()[-2])/100)
+    duration.append(eval(lines[-1].split()[-1]))
 
 order = numpy.arange(1, round+1)
 print(accuracy, duration,sep='\n')
